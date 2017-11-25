f1db4b34b5c3963e1fb05f31e2897eb4f4d0bd4a
kourb <i1954@xe.am>
2017-11-25 09:59:00 +0300

    General cleanup

--- test/simpletest.js
+++ test/simpletest.js
@@ -1,10 +1,10 @@
 //// Shortcuts
-	const {log} = require('./shortcuts.js')
+	const {log} = require('../lib/shortcuts.js')
 
 
 //// Imports
-	const {VEBT, VEBX} = require('./vebt.js')
-	const {getRandomArbitrary, getRandomInt, fibonacci} = require('./utilites.js')
+	const {VEBT, VEBX} = require('../lib/vebt.js')
+	const {getRandomArbitrary, getRandomInt, fibonacci} = require('../lib/utilites.js')
 
 
 //// Test config
@@ -17,20 +17,25 @@
 //// Simple Test
 	let Fibonacci = fibonacci(FIB_AMOUNT)
 	let Tree = new VEBX(VEBT.offerK(Fibonacci))
+	
 	// Filling <Tree> with random Ints from 1 to 1024
 	let c = RAND_AMOUNT; while(c--){
 		Tree.insert(getRandomInt(1,1024))
 	}
+
 	// Filling <Tree> with fibonacci sequence
 	Tree.insert(Fibonacci)
+
 	// Printing Fibonacci sequence
 	log('---- Fibonacci sequence ----')
 	log(Fibonacci)
 	log('--------------\n')
+
 	// Printing <Tree>
 	log('---- Tree ----')
 	log(Tree)
 	log('--------------\n')
+
 	// Finding random number form <Tree> and its neighbourhood 
 	let number = Fibonacci[getRandomInt(0, FIB_AMOUNT-1)]
 	let tExist = Tree.find(number) ? '✔' : '✘'
